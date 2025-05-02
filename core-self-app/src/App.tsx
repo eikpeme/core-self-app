f224775d63145cc1e5396f12a0c4a26c85f48ef0
Eik <etty.ikpeme@gmail.com>
2025-05-01 20:22:44 -0500

    Initial commit

---
+++ core-self-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+import CoreSelf from './components/CoreSelf';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <CoreSelf />
+    </div>
+  );
+}
+
+export default App;
+
